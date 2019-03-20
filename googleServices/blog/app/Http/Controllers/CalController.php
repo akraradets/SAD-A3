74e7fe61b98783dee233066b72db02079d98b6e7
akraradets <akraradets@gmail.com>
2019-03-21 01:22:29 +0700

    calCal done

--- googleServices/blog/app/Http/Controllers/CalController.php
+++ googleServices/blog/app/Http/Controllers/CalController.php
@@ -25,7 +25,24 @@ class CalController extends Controller
      */
     public function cal(Request $request)
     {
-        return $request->input("code");
-        return $request->all();
+        $returnArray = array(
+            "code" => "1",
+            "msg" => "",
+            "cal" => ""
+        );
+        if(empty($request->input("code"))){
+            $returnArray["msg"] = "<code> is empty";
+            return $returnArray;
+        }
+        if(empty($request->input("unit"))){
+            $returnArray["msg"] = "<unit> is empty";
+            return $returnArray;
+        }
+        $code = $request->input("code");
+        $unit = $request->input("unit");
+        $cal = strrev($code) * $unit;
+        $returnArray["code"] = "0";
+        $returnArray["cal"] = $cal;
+        return $returnArray;
     }
 }
